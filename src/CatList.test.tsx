641773eb5595ae9bcdde49c7ab58c53efd4dd5b7
Jyothish tj <jyothish.thadathil-janardhanan-pillai@capgemini.com>
2024-09-06 09:33:14 +1000

    Unit testing added

---
+++ src/CatList.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CatList from './CatList';
+
+let fetchMock: any = undefined;
+// Define the mock response data
+const mockData = [
+  {
+    "name": "Bob",
+    "gender": "Male",
+    "age": 23,
+    "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
+  },
+  {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
+  {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
+  {
+    "name": "Fred",
+    "gender": "Male",
+    "age": 40,
+    "pets": [
+      {"name": "Tom", "type": "Cat"},
+      {"name": "Max", "type": "Cat"},
+      {"name": "Sam", "type": "Dog"},
+      {"name": "Jim", "type": "Cat"}
+    ]
+  },
+  {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
+  {
+    "name": "Alice",
+    "gender": "Female",
+    "age": 64,
+    "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
+  }
+];
+
+
+beforeEach(() => {
+  fetchMock = jest.spyOn(global, "fetch")
+  .mockImplementation(catDetailsFetchMock);
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+
+export const catDetailsFetchMock = () => Promise.resolve({
+  ok: true,
+  status: 200,
+  json: async () => mockData
+  } as Response);
+
+
+test('renders cats sorted by owner gender', async () => {
+  render(<CatList />);
+
+  // Wait for the component to fetch and display data
+  await waitFor(() => {
+    // Check if the headers for genders are present
+    expect(screen.getByText('Male')).toBeInTheDocument();
+    expect(screen.getByText('Female')).toBeInTheDocument();
+
+    // Check if the cats are listed in alphabetical order
+     expect(screen.getByText('Jim')).toBeInTheDocument();
+    expect(screen.getByText('Max')).toBeInTheDocument();
+    expect(screen.getByText('Tom')).toBeInTheDocument();
+    expect(screen.getByText('Simba')).toBeInTheDocument();
+    expect(screen.getByText('Tabby')).toBeInTheDocument();
+  });
+});
